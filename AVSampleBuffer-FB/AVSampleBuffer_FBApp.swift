b26f6a7cc0eee561ca976727d1b38292a1d47502
Chronos2500 <kuronosusya@outlook.com>
2025-06-28 23:30:37 +0900

    Initial Commit

---
+++ AVSampleBuffer-FB/AVSampleBuffer_FBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVSampleBuffer_FBApp.swift
+//  AVSampleBuffer-FB
+//  
+//  Created by Chronos2500 on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct AVSampleBuffer_FBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVSampleBuffer-FB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AVSampleBuffer-FB
+//  
+//  Created by Chronos2500 on 2025/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
